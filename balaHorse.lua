2d202b51016a35419bc14da9aadf1feecb4fa6f1
GeorgeTheRat <earth.guy422@gmail.com>
2026-08-27 21:34:48 -0400

    Update balaHorse.lua

--- balaHorse.lua
+++ balaHorse.lua
@@ -1,8 +1,8 @@
 --- STEAMODDED HEADER
---- MOD_NAME: BalaGay
+--- MOD_NAME: FullHorse
 --- MOD_ID: balagay
---- MOD_AUTHOR: [Rat]
---- MOD_DESCRIPTION: Gay-ifies the Straights!
+--- MOD_AUTHOR: [GeorgeTheRat]
+--- MOD_DESCRIPTION: Horse House
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
@@ -10,19 +10,19 @@
 
 local function init()
     G.localization.misc.poker_hands['Flush House'] = "Flush Horse"
-    G.localization.misc.poker_hands['Full House'] = "Full horse"
+    G.localization.misc.poker_hands['Full House'] = "Full Horse"
 
     -- fix for Runner
     G.localization.descriptions.Joker["j_runner"].text = {
         "Gains {C:chips}+#2#{} Chips",
         "if played hand",
-        "contains a {C:attention}Gay{}",
+        "contains a {C:attention}Straight{}",
         "{C:inactive}(Currently {C:chips}+#1#{C:inactive} Chips)"
     }
 
     -- fix for Shortcut
     G.localization.descriptions.Joker["j_shortcut"].text = {
-        "Allows {C:attention}Gays{} to be",
+        "Allows {C:attention}Straight{} to be",
         "made with gaps of {C:attention}1 rank",
         "{C:inactive}(ex: {C:attention}10 8 6 5 3{C:inactive})" 
     }
@@ -30,7 +30,7 @@ local function init()
     -- fix for Four Fingers
     G.localization.descriptions.Joker["j_four_fingers"].text = {
         "All {C:attention}Flushes{} and",
-        "{C:attention}Gays{} can be",
+        "{C:attention}Straight{} can be",
         "made with {C:attention}4{} cards"   
     }
 
@@ -38,11 +38,11 @@ local function init()
     G.localization.descriptions.Joker["j_superposition"].text = {
         "Create a {C:tarot}Tarot{} card if",
         "poker hand contains an",
-        "{C:attention}Ace{} and a {C:attention}Gay{}",
+        "{C:attention}Ace{} and a {C:attention}Straight{}",
         "{C:inactive}(Must have room)"
     }
 
-    sendDebugMessage("BalaGay :: Successfully Horse-ified the Houses!")
+    sendDebugMessage("BalaHorse :: Successfully Horse-ified the Houses!")
 end
 
 if SMODS.current_mod then
